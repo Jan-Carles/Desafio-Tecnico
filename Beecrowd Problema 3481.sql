6307457e8aa95a84cb7bd6e966a473962efdbece
Jan Carles <jancarles25@gmail.com>
2025-11-05 15:46:02 -0300

    Refactor SQL comments and enhance query clarity
    Updated comments for clarity and added explanations for node types.

--- Beecrowd Problema 3481.sql
+++ Beecrowd Problema 3481.sql
@@ -4,21 +4,22 @@
 
 --Só é folha (LEAF) quando o pointer é NULL
 --Só é nó interno (INNER) se o pointer não for NULL
---Só é folha (LEAF) se o o pointer for NULL
+--Raiz (ROOT) só ver na imagem da árvore
 --Parece uma árvore B
 SELECT 
   resultado.node_id,
-  resultado.categoria AS tipo_no
+  resultado.categoria AS tipo_no --criando uma coluna temporária só para exibir oss ROOT, INNER< LEAF
 FROM (
   SELECT 
     n.node_id,
     CASE
-      WHEN n.node_id = 50 THEN 'ROOT'
-      WHEN n.pointer IS NOT NULL THEN 'INNER'
-      WHEN n.pointer IS NULL THEN 'LEAF'
+      WHEN n.node_id = 50 THEN 'ROOT' -- Se o node_id for 50, é nó raiz (ROOT)
+      WHEN n.pointer IS NOT NULL THEN 'INNER' --Se pointer não for NULL, é nó interno(INNER)
+      WHEN n.pointer IS NULL THEN 'LEAF' --Se o pointer for NULL, é folha (LEAF)
       ELSE NULL
     END AS categoria
-  FROM nodes AS n
-  GROUP BY n.node_id, n.pointer
+  FROM nodes AS n --renomeando
+  GROUP BY n.node_id, n.pointer --Agrupa os registros por node_id e pointer
 ) AS resultado
-ORDER BY resultado.node_id;
+ORDER BY resultado.node_id; --Ordena de forma crescente
+
